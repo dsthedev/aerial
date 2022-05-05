73871a82c4971bd2e677982c4d7a9e6d0ad629bc
dsthedev <dsopiarz@gmail.com>
2022-05-04 21:04:26 -0500

    oops

--- web/src/pages/AerialPage/AerialPage.js
+++ web/src/pages/AerialPage/AerialPage.js
@@ -3,8 +3,8 @@ import { MetaTags } from '@redwoodjs/web'
 
 const AerialPage = () => {
   let displayURL = (process.env.DISPLAY_URL) ? process.env.DISPLAY_URL : window.location.hostname
-  let tagline = (process.env.TAGLINE_DESC == 2) ? process.env.TAGLINE_DESC : "Project On-Hold | Domain Unavailable"
-  let devURL = (process.env.DEV_URL ==1) ? process.env.DEV_URL : "https://github.com/dsthedev?tab=repositories"
+  let tagline = (process.env.TAGLINE_DESC) ? process.env.TAGLINE_DESC : "Project On-Hold | Domain Unavailable"
+  let devURL = (process.env.DEV_URL) ? process.env.DEV_URL : "https://github.com/dsthedev?tab=repositories"
   let emailHREF = (process.env.ADMIN_EMAIL) ? "mailto://" + process.env.ADMIN_EMAIL + "?Subject=Inquiry of " + process.env.DISPLAY_URL : ""
 
   return (
